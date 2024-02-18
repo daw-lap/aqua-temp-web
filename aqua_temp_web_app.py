8cacfaac1fad0092e62d4a2c4b6bf72f4fc5c744
daw-lap <dawid.lapczynski@gmail.com>
2024-02-18 16:23:23 +0100

    implement viewing temperature from mqtt broker

--- aqua_temp_web_app.py
+++ aqua_temp_web_app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+from time import sleep
+import json
 
 app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = '192.168.0.170'
+app.config['MQTT_BROKER_PORT'] = 1883
+
+socketio = SocketIO(app)
+mqtt = Mqtt(app)
+
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    print("connected to broker")
+    sleep(1)
+    mqtt.subscribe('client/0')
+    mqtt.subscribe('client/1')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    received_message_json = message.payload.decode()
+    received_temp = json.loads(received_message_json)["temp"]
+    if message.topic == "client/0":
+        socketio.emit('temp_living_room', received_temp)
+    elif message.topic == "client/1":
+        socketio.emit('temp_bedroom', received_temp)
+
 
 @app.route('/')
-def hello():
-    return 'Welcome'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
